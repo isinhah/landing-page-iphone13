9848ecb51e5de94d758c0a49af9ef23d04f3cf6a
Isabel Henrique <6isinha6@gmail.com>
2023-05-13 18:17:53 -0300

    Mudei os comentários do código

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function imgSlider(e) {
-  //Função que muda o celular assim que clica no botão de cor
+  //Função que muda a imagem do celular assim que clica no botão de cor
   document.querySelector("#phone").src = e;
 }
 
-//Função de mudar a cor do clip-path, de acordo com o celular escolhido
+//Função de mudar a cor do clip-path
 function circleChange(color) {
   const circle = document.querySelector(".circle");
   circle.style.background = color;
